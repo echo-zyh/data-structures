6d9a8d31201be7b48c2548c2952ffb03c93e44b4
echo-zyh <1033009841@qq.com>
2020-07-28 21:50:34 +0800

    单向链表

---
+++ 2020 7-28/Node.java
@@ -0,0 +1,13 @@
+/**
+ * @Author: echo Zhao
+ * @Descciption:
+ * @Date: Created in 15:50 2020/7/28 0028
+ * @Modified By:
+ */
+public class Node{
+    public int data;
+    public Node next;
+    public Node(int data){
+        this.data = data;
+    }
+}
